3d8172a50e5874ec1f26cb8a8ed2e9cdf04a5b21
thesnpzone <108673857+thesnpzone@users.noreply.github.com>
2022-07-05 21:32:33 +0530

    Add files via upload

---
+++ src/js/main.js
@@ -0,0 +1,54 @@
+// nav bar section start
+
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
+
+
+
+// nav bar section end
+
+// go to top
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+function onReady(callback) {
+    var intervalID = window.setInterval(checkReady, 3000);
+
+    function checkReady() {
+        if (document.getElementsByTagName('body')[0] !== undefined) {
+            window.clearInterval(intervalID);
+            callback.call(this);
+        }
+    }
+}
+
+function show(id, value) {
+    document.getElementById(id).style.display = value ? 'block' : 'none';
+}
+
+onReady(function() {
+    show('page', true);
+    show('loading', false);
+});
